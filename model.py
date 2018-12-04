8504434ad0686c21422ece10080d8542420d669f
StraetenMax <maxstraeten@yahoo.com>
2018-12-04 09:50:55 +0100

    creation de la première méthode

--- model.py
+++ model.py
@@ -1,6 +1,13 @@
-class Agent():
-    pass
+# class Agent():
+#   pass
 
-first_agent = Agent()
-second_agent = Agent()
-print(first_agent, second_agent)
+# first_agent = Agent()
+# second_agent = Agent()
+# print(first_agent, second_agent)
+
+class Agent:
+    def say_hello(self, first_name):
+        return "Bien le bonjour " + first_name + " !"
+
+agent = Agent()
+print(agent.say_hello("Céline"))
