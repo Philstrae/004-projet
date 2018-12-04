2e990a8f29d93dd7cfb9c8229f578e9aa627e9c4
StraetenMax <maxstraeten@yahoo.com>
2018-12-04 10:05:32 +0100

    creation des premiers attributs

--- model.py
+++ model.py
@@ -8,6 +8,12 @@
 class Agent:
     def say_hello(self, first_name):
         return "Bien le bonjour " + first_name + " !"
+    
+    def __init__(self, agreeableness):
+        self.agreeableness = agreeableness  
 
 agent = Agent()
 print(agent.say_hello("Céline"))
+
+first_agent = Agent(0)
+print(first_agent.agreeableness)
