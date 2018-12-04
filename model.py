e187e2cfb0ed0740e2316c10338de0dd8dc7b49e
StraetenMax <maxstraeten@yahoo.com>
2018-12-04 11:40:29 +0100

    mettre en place les attributs d'objets à partir d'une base

--- model.py
+++ model.py
@@ -7,15 +7,28 @@ import json
 # second_agent = Agent()
 # print(first_agent, second_agent)
 
-class Agent:
-    def say_hello(self, first_name):
-        return "Bien le bonjour " + first_name + " !"
-    
-    def __init__(self, agreeableness):
-        self.agreeableness = agreeableness  
+#class Agent:
+#    def say_hello(self, first_name):
+#       return "Bien le bonjour " + first_name + " !"
 
 #agent = Agent()
-#print(agent.say_hello("Céline"))
+#print(agent.say_hello("Céline"))      
+    
+#    def __init__(self, agreeableness):
+#        self.agreeableness = agreeableness  
+class Agent():
+     def __init__(self, agent_attributes):
+            self.agreeableness = agent_attributes["agreeableness"]
+            self.neuroticism = agent_attributes["neuroticism"]
 
-first_agent = Agent(0)
-print(first_agent.agreeableness)
+
+agent_attributes = {"neuroticism": -0.0739192627121713, "language": "Shona", "latitude": -19.922097800281783, "country_tld": "zw", "age": 12, "income": 333, "longitude": 29.798455535838603, "sex": "Male", "religion": "syncretic", "extraversion": 1.051833688742943, "date_of_birth": "2005-01-10", "agreeableness": 0.1441229877537559, "id_str": "LB3-3Cl", "conscientiousness": 0.2419104411765549, "internet": "false", "country_name": "Zimbabwe", "openness": -0.024607605122172617, "id": 6636726630}     
+
+
+first_agent = Agent(agent_attributes)
+print(
+    first_agent.agreeableness, 
+    first_agent.neuroticism
+    )
+
+
