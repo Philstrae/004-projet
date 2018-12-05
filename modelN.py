a3d4c5d947089e74d0967c8dcdad70dd2126290f
StraetenMax <maxstraeten@yahoo.com>
2018-12-05 12:53:28 +0100

    création d'un fichier pour établir la position de l'agent

---
+++ modelN.py
@@ -0,0 +1,18 @@
+# -*- coding:utf8 -*-
+
+import json
+
+class Agent:
+
+    def say_hello(self, first_name):
+        return "Bien le bonjour " + first_name
+
+    def __init__(self, **agent_attributes):
+        for attr_name, attr_value in agent_attributes.items():
+                setattr(self, attr_name, attr_value)
+
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        agent = Agent(**agent_attributes)
+        print(agent.agreeableness, agent.neuroticism)
+main()            
