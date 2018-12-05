218f3a73ca88bf007fc3c16da219e6a87aeda7c5
StraetenMax <maxstraeten@yahoo.com>
2018-12-05 13:36:50 +0100

    mise en place de la class position test de la latitude

--- modelN.py
+++ modelN.py
@@ -7,7 +7,8 @@ class Agent:
     def say_hello(self, first_name):
         return "Bien le bonjour " + first_name
 
-    def __init__(self, **agent_attributes):
+    def __init__(self, position, **agent_attributes):
+        self.position = position
         for attr_name, attr_value in agent_attributes.items():
                 setattr(self, attr_name, attr_value)
 
@@ -19,6 +20,10 @@ class Position:
 
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
-        agent = Agent(**agent_attributes)
-        print(agent.agreeableness, agent.neuroticism)
+        latitude = agent_attributes.pop('latitude')
+        longitude = agent_attributes.pop('longitude')
+        position = Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
+        print(agent.position.latitude)
+
 main()            
