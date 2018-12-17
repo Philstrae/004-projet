3d39661238dc3f14925cbd7abaafaa985fa982c2
StraetenMax <maxstraeten@yahoo.com>
2018-12-17 14:24:22 +0100

    réalisation de la grille

--- modelZoneNew.py
+++ modelZoneNew.py
@@ -28,10 +28,27 @@ class Position:
         return self.latitude_degrees * math.pi / 180          
 
 class Zone:
+
+    MIN_LONGITUDE_DEGREES = -180
+    MAX_LONGITUDE_DEGREES = 180
+    MIN_LATITUDE_DEGREES = -90
+    MAX_LATITUDE_DEGREES = 90
+    WIDTH_DEGREES = 1
+    HEIGHT_DEGREES = 1
+
     def __init__(self, corner1, corner2):
         self.corner1 = corner1
         self.corner2 = corner2
-        self.inhabi
+        self.inhabitants = 0
+
+    def initialize_zones(self):
+        for latitude in range(self.MIN_LATITUDE_DEGREES, self.MAX_LATITUDE_DEGREES)
+            for longitude in range(self.MIN_LONGITUDE_DEGREES, self.MAX_LONGITUDE_DEGREES, WIDTH_DEGREES)
+                bottom_left_corner = Position(longitude, latitude)
+                top_right_corner = Position(longitude + self.WIDTH_DEGREES, latitude + self.HEIGHT_DEGREES)
+
+
+
 
 def main():
     for agent_attributes in json.load(open("agents-100k.json")):
